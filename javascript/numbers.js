7bab98128e3bb7168590c7b2ec973faa79198ff6
Aditya deshmukh <65219206+Aditya664@users.noreply.github.com>
2021-08-22 17:53:47 +0000

    new

--- javascript/numbers.js
+++ javascript/numbers.js
@@ -7,3 +7,13 @@ const pants = 2.445;
 
 console.log(num);
 console.log(pants);
+
+const add = num+pants;
+const sub = num-pants;
+const mult = num*pants;
+const div = num/pants;
+
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
