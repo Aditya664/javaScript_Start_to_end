1967ef0bb9d12a6278eb6fad335af705d33a5e0f
Aditya deshmukh <65219206+Aditya664@users.noreply.github.com>
2021-08-22 16:56:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//variables 
+
+let name="Aditya";
+
+console.log(name);
---
+++ javascript/variable.js
@@ -0,0 +1,14 @@
+//variables 
+
+let name="Aditya";
+
+console.log(name);
+
+// variable assigne latter
+
+//variables 
+
+let name;
+name = "Aditya";
+
+console.log(name);
