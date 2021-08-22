bb2c820ce3dcfc76973267de0095c054366c6c9b
Aditya deshmukh <65219206+Aditya664@users.noreply.github.com>
2021-08-22 17:45:40 +0000

    new

--- javascript/string-concat.js
+++ javascript/string-concat.js
@@ -1,5 +1,6 @@
 let frname = "adi";
 let faname = "sandip";
 let srname = "deshmukh";
+let lname = frname + ' ' +faname+ ' ' +srname;
 
-console.log("Hello my name is:" + frname + faname + srname);
+console.log('Hello my name is:'+lname);
